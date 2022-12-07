1a18347a01c474ceb6cf23d9d3f9f7f66847e61e
PlutoThePlanet <pmortensen0916@gmail.com>
2022-12-07 14:10:12 -0800

    more setup

--- simulation.py
+++ simulation.py
@@ -1,18 +1,63 @@
-#
-#
-#
-#
-#
-#
+# Dynamic routing mechanism design in faulty network
+# The team will simulate a mesh network where nodes and links may fail (Figure 5). Nodes and links may fail
+# intermittently, as an input to the simulation, each node and link will have a certain probability to fail.
+# When such failure occurs, the network must adapt and re-route to avoid the faulty link/node.
 
-#library imports
+# code compiles and runs
+# code is doccumented
 
-#Declare node + edge data globally here
+# report explains  the functionality of the protocol
+# novel contibution
+# results and analysis of the results
+
+
+
+# Paige Mortensen and Marissa Floam
+# CPE 400
+# Final Project
+# Faulty Node Networking
+
+
+# library imports
+
+# Declare node + edge data globally here
+# Default graph structure
+nodes = [['a', 0.02], ['b', 0.05], ['c', 0.1], ['d', 0.01], ['e', 0.01], ['f', 0.15]] # [node, failureProb]
+edges = [['a', 'b', 0.01], ['a', 'c', 0.01], ['b', 'd', 0.01], ['c', 'd', 0.01], ['d', 'e', 0.01], ['d', 'f', 0.01], ['e', 'f', 0.01]] # [pt1, pt2, failureProb]
+# nodeFailureProb = []
+# edgeFailureProb = []
 
 #Graph Class
 class Graph:
 	#attributes: nodes, edges, failure likelihoods, distance, delay
 	#functions: add node, remove node, add edge, remove edge, display/compare?
+    
+    # constructor
+    # def __init__(self):
+        # 
+    
+    def addNode():
+        # name your node:
+        # what failure prob. would you like it to have (0-1)
+        # name an adjacent node to connect your new node to
+        
+    def deleteNode():
+        # what node do would you like to delete: 
+        # delete node from nodes[]
+        # iterate through edges[] and delete entire edge when encountered
+        
+    def addEdge():
+        # name your first node: 
+        # name your second node: 
+        # what failure prob would you like it to have: 
+        
+    def deleteEdge():
+        # delete the edge between what and what nodes: p, q
+        # iterate through edges[] and if contains both p and q, delete entire edge
+        
+    # def displayGraph():
+        # iterate through edges[] and nodes[] and simply print ?
+        
 
 #Loads all data into graph from graph class
 def loadGraph():
